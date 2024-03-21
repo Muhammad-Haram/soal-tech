0b3171f8f8e3c48bc9008e98bb6c7f4faf8793c7
Muhammad Haram <haramh643@gmail.com>
2024-03-22 02:48:22 +0500

    some changes in nav bar inner section

--- Script.js
+++ Script.js
@@ -16,3 +16,29 @@ closeBtn.addEventListener("click", () => {
         navInfo.classList.remove("show");
     }
 })
+
+// inner navbar / tab section
+
+var allBtns = document.querySelectorAll(".tab-btn");
+
+for (let b = 0; b < allBtns.length; b++) {
+    allBtns[b].addEventListener("click", function () {
+        const data = this.dataset.tab;
+        const ids = document.getElementById(data);
+
+        var allContent = document.querySelectorAll(".tabcontent");
+
+        for (let s = 0; s < allContent.length; s++) {
+            allContent[s].style.display = "none";
+        }
+
+        for (let s = 0; s < allBtns.length; s++) {
+            allBtns[s].classList.remove("active");
+        }
+
+        this.classList.add("active");
+        ids.style.display = "block";
+    });
+}
+
+document.querySelector(".tab-btn").click();
